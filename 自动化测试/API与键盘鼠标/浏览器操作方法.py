8cbb5b11684da9a38e287abafe6d35a627c570ba
“stearm210” <pyzxwoo@163.com>
2024-03-09 14:29:50 +0800

    修改2

--- 自动化测试/API与键盘鼠标/浏览器操作方法.py
+++ 自动化测试/API与键盘鼠标/浏览器操作方法.py
@@ -1,4 +1,26 @@
 
+from time import sleep
+from selenium import webdriver
+#实例化对象
+driver=webdriver.Chrome()
+driver.get('www.baidu.com')
+
+#1.最大化窗口maximize_window()模拟浏览器最大化按钮
+'''
+如果能够在打开页面时，全屏显示页面，就能尽最大可能加载更多页面元素
+提高可定位性
+'''
+#driver.maximize_window()
+
+#2.设置浏览器窗口，set_window_size(width,height)设置浏览器宽、高（像素点）
+driver.set_window_size(300,300)
+#上面用于查看页面是否可以自适应在web和app端切换使用
+
+#3.set_windows_position(x,y)设置浏览器窗口位置，设置浏览器位置
+
+
+sleep(3)
+driver.quit()
 
 
 
