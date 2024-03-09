a4266356c8d4229c7f7e065c8bc413ed63fd447e
“stearm210” <pyzxwoo@163.com>
2024-03-09 16:10:18 +0800

    修改2

--- 自动化测试/API与键盘鼠标/获取元素信息方法.py
+++ 自动化测试/API与键盘鼠标/获取元素信息方法.py
@@ -12,13 +12,24 @@ print('目标元素是否显示：',span.is_displayed())
 
 
 #5.is_enabled()判断元素是否可用
+#说明，此方法用于判断目标元素是否可以进行交互使用
+can_btn=driver.find_element_by_id('cancelA')
+print('目标元素是否可用',can_bin.is_enabled())
 
 
 #6.is_selected()判断元素是否选中，用来检查复选框或者单选按钮是否选中
-
-
+#场景：购物车，不全选商品，不让结算
+check=driver.find_element_by_id('lyA')
+print('目标元素是否被选中:',check.is_selected())
 '''
 
+'''
+#拓展
+if check.is_selected():#选中判断
+   pass
+if not check.is_selected():#未选中的判断
+   pass
+'''
 
 
 
